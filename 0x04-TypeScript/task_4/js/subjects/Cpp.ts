5ca35923081e91820e24739127671f40d6dce352
Eddy Ckedar <eddykedar@gmail.com>
2025-07-10 15:23:50 +0200

    Create Cpp.ts

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,21 @@
+// Task 9: Namespace & Declaration merging - Cpp class
+
+namespace Subjects {
+  // Declaration merging - add experienceTeachingC to Teacher interface
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+
+  export class Cpp extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for Cpp";
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher.experienceTeachingC && this.teacher.experienceTeachingC > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return "No available teacher";
+    }
+  }
+}
